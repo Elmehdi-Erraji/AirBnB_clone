0ccabbc410640fc6cf2df87b974951440216e3f0
Nizar-Bahajoub <116908630+Nizar-Bahajoub@users.noreply.github.com>
2023-08-13 20:33:33 +0100

    models added

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
---
+++ models/base_model.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+base_model module
+"""
+import uuid
+from datetime import datetime
+import models
+
+
+class BaseModel:
+    """BaseModel Class"""
+    
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.fromisoformat(value))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+        models.storage.new(self)
+
+    def __str__(self):
+        """String representation of BaseModel"""
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """Updates updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        models.storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary of all keys/values of __dict__ of the instance"""
+        my_dict = self.__dict__.copy()
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return (my_dict)
